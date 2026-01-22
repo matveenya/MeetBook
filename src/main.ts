eb4ee89466b04574ba13824be42e951e85cf5b2f
Roman Marveenya <romanmatveena@gmail.com>
2026-01-22 11:56:37 +0100

    refactor: replace from html element to PrimeVue component

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 import './style.css';
 import App from './App.vue';
 import { auth } from './auth';
 
 const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.my-app-dark',
+    },
+  },
+});
 app.use(auth);
 app.mount('#app');
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -4,27 +4,34 @@
       <h1 class="text-[#333] text-3xl font-medium mb-12">Welcome</h1>
 
       <form class="w-full space-y-4" @submit.prevent>
-        <input
-          type="email"
-          placeholder="Email"
-          class="w-full pl-12 pr-4 py-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-[#3f4bb0] focus:border-transparent transition"
-        />
+        <div class="flex flex-col gap-1">
+          <InputText
+            type="email"
+            placeholder="Email"
+            fluid
+            class="w-full! pl-12! pr-4! py-3! border! border-gray-300! rounded-xl! focus:outline-none! focus:ring-2! focus:ring-[#3f4bb0]! focus:border-transparent! transition!"
+          />
+        </div>
 
-        <input
-          type="password"
-          placeholder="Password"
-          class="w-full pl-12 pr-12 py-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-[#3f4bb0] focus:border-transparent transition"
-        />
+        <div class="flex flex-col gap-1">
+          <Password
+            placeholder="Password"
+            :feedback="false"
+            toggleMask
+            fluid
+            inputClass="w-full! pl-12! pr-4! py-3! border! border-gray-300! rounded-xl! focus:outline-none! focus:ring-2! focus:ring-[#3f4bb0]! focus:border-transparent! transition!"
+          />
+        </div>
 
         <div class="text-right">
           <a href="#" class="text-[#7e8ae5] text-sm hover:underline">Forgot password?</a>
         </div>
 
-        <button
-          class="w-full bg-[#3f4bb0] text-white py-3 rounded-full font-medium hover:bg-[#343e94] transition mt-6 shadow-md"
-        >
-          Log in
-        </button>
+        <Button
+          label="Log in"
+          fluid
+          class="w-full! bg-[#3f4bb0]! text-white! py-3! rounded-full! font-medium! hover:bg-[#343e94]! transition! mt-6! shadow-md!"
+        />
       </form>
 
       <div class="w-full flex items-center my-8">
@@ -33,32 +40,39 @@
         <div class="flex-1 h-px bg-gray-200"></div>
       </div>
 
-      <div class="w-full flex space-x-4 mb-10">
-        <button
+      <div class="w-full mb-10">
+        <Button
           @click="loginWithGoogle"
-          class="flex-1 flex items-center justify-center py-2.5 border border-[#7e8ae5] rounded-full hover:bg-gray-50 transition"
+          label="Google"
+          variant="outlined"
+          fluid
+          class="flex-1! flex! items-center! justify-center! py-2.5! border! border-[#7e8ae5]! rounded-full! hover:bg-gray-50! transition!"
         >
           <img
             src="https://www.gstatic.com/images/branding/product/1x/gsa_512dp.png"
             alt="Google"
-            class="w-5 h-5 mr-2"
+            class="w-5 h-5"
           />
-          <span class="text-[#3f4bb0] font-medium text-sm">Google</span>
-        </button>
+          <span class="text-[#3f4bb0]! font-medium! text-sm!">Google</span>
+        </Button>
       </div>
 
-      <p class="text-gray-500 text-sm mb-4">Have no account yet?</p>
-      <button
-        class="w-full border border-[#7e8ae5] text-[#3f4bb0] py-3 rounded-full font-medium hover:bg-gray-50 transition"
-      >
-        Registration
-      </button>
+      <p class="text-gray-500 text-sm mb-4 text-center">Have no account yet?</p>
+      <Button
+        label="Registration"
+        variant="outlined"
+        fluid
+        class="w-full! border! border-[#7e8ae5]! text-[#3f4bb0]! py-3! rounded-full! font-medium! hover:bg-gray-50! transition!"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useAuth } from 'vue-auth3';
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+import Button from 'primevue/button';
 
 const auth = useAuth();
 
