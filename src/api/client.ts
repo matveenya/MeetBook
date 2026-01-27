8b50f27240649eb6e80d05eeafe790ede4fc86e3
Roman Marveenya <romanmatveena@gmail.com>
2026-01-27 12:07:46 +0100

    feat: add a service for auth logic and interceptors

---
+++ src/api/client.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosError } from 'axios';
+import { auth } from '../auth';
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:3001',
+  withCredentials: true,
+});
+
+apiClient.interceptors.response.use(
+  response => response,
+  (error: AxiosError) => {
+    if (error.response) {
+      const status = error.response.status;
+
+      if (status === 401) {
+        auth.logout({ redirect: '/login' });
+      }
+
+      if (status === 403) {
+        console.error('Доступ запрещен: у вас недостаточно прав.');
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
